80ee435f5fd68c436f0e251250962261279526ed
Joseph Lenox <lenox.joseph@gmail.com>
2011-11-25 21:30:30 -0600

    Added function to copy 1d array to gpu memory.

--- gpuiscas.cu
+++ gpuiscas.cu
@@ -71,3 +71,22 @@ int* gpuLoadVectors(int** input, size_t width, size_t height) {
 	}
 	return tgt;
 }
+int* gpuLoad1DVector(int* input, size_t width, size_t height) {
+	int *tgt, *row;
+	HANDLE_ERROR(cudaMalloc(&tgt, sizeof(int)*(width+1)*(height+1)));
+	for (int i = 0; i < height; i++) {
+		row = (int*)((char*)tgt + i*(width)*sizeof(int));
+		cudaMemcpy(row, input[i],sizeof(int)*(width+1),cudaMemcpyHostToDevice);
+#ifndef NDEBUG
+		int *tmp = (int*)malloc(sizeof(int)*width);
+		for (int i =0; i <= width; i++)
+			tmp[i] = -1;
+		cudaMemcpy(tmp, row, sizeof(int)*(width+1),cudaMemcpyDeviceToHost);
+		for (int j = 0; j <= width; j++) {
+			assert(input[i][j]==tmp[j]);
+		}
+		free(tmp);
+#endif // debugging memory check and assertion
+	}
+	return tgt;
+}
--- main.cc
+++ main.cc
@@ -10,18 +10,20 @@
 int procgate(NODE gnode, int settle); 
 int main(int argc, char ** argv) {
 	FILE *fisc, *fvec;
-	int *dres, *fans;
+	int *dres, *fans, *vec;
 	GPUNODE *dgraph;
 	LINE *dlines;
 	int** res;
 	NODE graph[Mnod];
 	LINE lgraph[Mnod];
 	GPUNODE_INFO test;
-	int lcnt, ncnt; // count of lines in the circuit
-	int PATTERNS = 4;
+	int vcnt, lcnt, ncnt, pis = 0; // count of lines in the circuit
 
 	// Load circuit from file
 	fisc=fopen(argv[1],"r");
+	fvec=fopen(argv[2],"r");
+	vcnt = readVectors(&vec, fvec);
+
 	ncnt = ReadIsc(fisc,graph);
 	for (int i = 0; i < Mnod; i++)
 		InitializeLines(lgraph, i);
@@ -36,19 +38,12 @@ int main(int argc, char ** argv) {
 	DPRINT("ID:\tType\n");
 	for(int i = 0; i < ncnt; i++) {
 		DPRINT(" %d:\t%d\n",i,test.graph[i].type);
+		if (graph[i].typ == INPT)
+			pis++;
 	}
-	int **vec = (int**)malloc(sizeof(int*)*4);
-	int vecA[5] = {1,0,0,1,1}; //v0
-	int vecB[5] = {0,1,0,0,0}; //v1
-	int vecC[5] = {1,0,1,0,1}; //v2
-	int vecD[5] = {0,0,1,1,1}; //v3
-	vec[0] = vecA;
-	vec[1] = vecB;
-	vec[2] = vecC;
-	vec[3] = vecD;
-	
-	res = (int**)malloc(sizeof(int*)*PATTERNS);
-	for (int i = 0; i < PATTERNS; i++) {
+
+	res = (int**)malloc(sizeof(int*)*vcnt);
+	for (int i = 0; i < vcnt; i++) {
 		res[i] = (int*)malloc(sizeof(int)*lcnt);
 		for (int j = 0; j < lcnt; j++) {
 			res[i][j] = 0;
@@ -64,15 +59,14 @@ int main(int argc, char ** argv) {
 		DPRINT("%d: %d\n", i,test.graph[i].offset);
 	}
 
-
-	dres = gpuLoadVectors(res, lcnt, PATTERNS);
-	int* dvec = gpuLoadVectors(vec, 5, 4);
+	dres = gpuLoadVectors(res, lcnt, vcnt);
+	int* dvec = gpu1DLoadVector(vec, pis, vcnt / pis);
 	ARRAY2D<int> inputArray = ARRAY2D<int>(dvec, 4, 5);
 	dgraph = gpuLoadCircuit(test.graph,ncnt);
 	dlines = gpuLoadLines(lgraph,lcnt);
 	fans = gpuLoadFans(test.offsets,test.max_offset);
 	loadLookupTables();
-	runGpuSimulation(ARRAY2D<int>(dres,PATTERNS,lcnt), inputArray, test.graph,ARRAY2D<GPUNODE>(dgraph,1,ncnt),ARRAY2D<LINE>(dlines,PATTERNS,lcnt),fans, 1);
+	runGpuSimulation(ARRAY2D<int>(dres,vcnt,lcnt), inputArray, test.graph,ARRAY2D<GPUNODE>(dgraph,1,ncnt),ARRAY2D<LINE>(dlines,vcnt,lcnt),fans, 1);
 	DPRINT ("Max Node ID: %d\tLines: %d\n",ncnt,lcnt);
 	PrintCircuit(graph,ncnt);
 //	PrintLines(lgraph,lcnt);
