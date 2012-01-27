ea23ec9c146682942833d09ac18f8031761f1ba3
Joseph Lenox <lenox.joseph@gmail.com>
2012-01-27 16:56:48 -0600

    Added debug information.

--- main.cc
+++ main.cc
@@ -1,3 +1,4 @@
+#include "utility.h"
 #include "ckt.h"
 #include "gpuckt.h"
 #include "gpudata.h"
@@ -7,17 +8,30 @@
 #include <iostream>
 int main(int argc, char ** argv) {
 	GPU_Circuit ckt;
+	timespec start, stop;
+	float elapsed = 0.0;
 	std::cerr << "Reading benchmark file " << argv[1] << "....";
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
 	ckt.read_bench(argv[1]);
-	std::cerr << "..complete." << std::endl;
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
+	elapsed = floattime(diff(start, stop));
+	std::cerr << "..complete. Took " << elapsed  << "ms" << std::endl;
 	std::cerr << "Copying circuit to GPU...";
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
 	ckt.copy(); // copy circuit to GPU
-	std::cerr << "..complete." << std::endl;
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
+	elapsed = floattime(diff(start, stop));
+	std::cerr << "..complete. Took " << elapsed  << "ms" << std::endl;
 	std::pair<size_t,size_t> vecdim = get_vector_dim(argv[2]);
 	GPU_Data vec(vecdim.first,vecdim.second);
+
 	std::cerr << "Reading vector file....";
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
 	read_vectors(vec, argv[2], vec.pitch());
-	std::cerr << "..complete." << std::endl;
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop);
+	elapsed = floattime(diff(start, stop));
+	std::cerr << "..complete. Took " << elapsed  << "ms" << std::endl;
+
 	std::cerr << "Initializing gpu memory for results...";
 	GPU_Data sim_results(vecdim.first,ckt.size()); // initializing results array for simulation
 	std::cerr << "..complete." << std::endl;
