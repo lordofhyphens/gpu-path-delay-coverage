2facc455f7e80fb2071667f05c463979fc94ee7e
Joseph Lenox <lenox.joseph@gmail.com>
2012-07-02 14:55:33 -0500

    Moved utility files to own repo.

--- util/variance_gen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-for i in `seq 1 1 30`; do ./fcount $1 $2 2> log-$i.txt; echo "$i of 30"; done
-for i in `seq 1 1 30`; do for j in `seq 1 1 30`; do diff -q log-$i.txt log-$j.txt; done;done
