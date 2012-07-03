39b3669385959a69e641aeaf9b95468ce20e9a30
Joseph Lenox <lenox.joseph@gmail.com>
2012-07-03 12:41:52 -0500

    Added desctructor to subcircuit

--- subckt.cc
+++ subckt.cc
@@ -1,17 +1,35 @@
 #include "subckt.h"
 
 SubCkt::SubCkt(const Circuit& ckt) : _ckt(ckt) {
+	_flat = NULL;
+	_gpu = NULL;
 	_levels = new std::vector<int>();
 	_subckt = new std::vector<int>();
+	for (int  i = 0; i <= _ckt.levels(); i++) {
+		_levels->push_back(0); // add more nodes to the levels vector if necessary
+	}
 }
 SubCkt::SubCkt(const Circuit& ckt, unsigned int node) : _ckt(ckt) {
+	_flat = NULL;
+	_gpu = NULL;
 	_levels = new std::vector<int>();
 	_subckt = new std::vector<int>();
+	for (int  i = 0; i <= _ckt.levels(); i++) {
+		_levels->push_back(0); // add more nodes to the levels vector if necessary
+	}
 	grow(node);
-	levelize();
 }
-
-std::string SubCkt::save() {
+SubCkt::~SubCkt() {
+	delete _levels;
+	delete _subckt;
+}
+bool SubCkt::operator<(const SubCkt& other) const {
+	return (*this < other.size());
+}
+bool SubCkt::operator<(int other) const {
+	return size() < other;
+}
+std::string SubCkt::save() const {
 	// dump the subckt to a space-separated file, followed by a newline.
 	std::stringstream ofile;
 	if (_subckt->size() > 0) {
@@ -24,25 +42,34 @@ std::string SubCkt::save() {
 }
 
 void SubCkt::load(const std::string& memfile) {
+	//std::cerr << "Subckt: "<< memfile << std::endl;
 	std::string data = memfile.substr(memfile.find_first_of(':') + 1);
 	std::stringstream t(memfile.substr(memfile.find_first_of(':')));
 	std::stringstream ifile(data);
 	t >> _ref_node;
 	int z;
 	while(!ifile.eof()) { ifile >> z; add(z);}
+	std::sort(_subckt->begin(), _subckt->end());
+	std::vector<int>::iterator it = unique(_subckt->begin(), _subckt->end());
+	_subckt->resize(it - _subckt->begin());
+	levelize();
+	_ref_node = _subckt->at(0);
 }
-SubCkt::~SubCkt() {
-	delete _levels;
-	delete _subckt;
-}
+
 void SubCkt::add(const Circuit& ckt, const int& n) {
-//	for (int i = this->_levels->size(); i <= ckt.at(n).level; i++) {
-//		_levels->push_back(0); // add more nodes to the levels vector if necessary
-//	}
 //	_levels->at(ckt.at(n).level) += 1;
 	_subckt->push_back(n);
 }
-
+void SubCkt::levelize() {
+	delete _levels;
+	_levels = new std::vector<int>();
+	for (int  i = 0; i <= _ckt.levels(); i++) {
+		_levels->push_back(0); // add more nodes to the levels vector if necessary
+	}
+	for (unsigned int i = 0; i < _subckt->size(); i++) {
+		_levels->at(_ckt.at(_subckt->at(i)).level) += 1;
+	}
+}
 void SubCkt::grow(unsigned int node) {
 	const NODEC& home_node = _ckt.at(node);
 	// starting from the home node, traverse through its fanins and fanouts, adding them one-by-one.
@@ -59,8 +86,6 @@ void SubCkt::grow(unsigned int node) {
 }
 
 void SubCkt::grow_recurse_back(unsigned int node) {
-	if ((int)node > _ckt.size()) { return; }
-	if (find(_subckt->begin(),_subckt->end(),node) != _subckt->end()) { return;}
 	const NODEC& home_node = _ckt.at(node);
 	add(node);
 	for (unsigned int i = 0; i < home_node.fin.size(); i++) {
@@ -69,8 +94,6 @@ void SubCkt::grow_recurse_back(unsigned int node) {
 }
 
 void SubCkt::grow_recurse_forward(unsigned int node) {
-	if ((int)node > _ckt.size()) { return; }
-	// avoid multiple node additions.
 	const NODEC& home_node = _ckt.at(node);
 	add(node);
 	for (unsigned int i = 0; i < home_node.fot.size(); i++) {
@@ -81,7 +104,9 @@ void SubCkt::grow_recurse_forward(unsigned int node) {
 int SubCkt::at(unsigned int n) {
 	return this->_subckt->at(n);
 }
-
+int SubCkt::levels() {
+	return _levels->size() - 1;
+}
 int SubCkt::levelsize(unsigned int n) {
 	if (n < this->_levels->size()) {
 		return this->_levels->at(n);
@@ -90,10 +115,11 @@ int SubCkt::levelsize(unsigned int n) {
 	}
 }
 
-int* SubCkt::gpu() {
-	int* z = new int[_subckt->size()];
+int* SubCkt::flat() {
+	int* z = new int[_subckt->size()+1];
 	for (unsigned int i = 0; i < _subckt->size(); i++)
 		z[i] = _subckt->at(i);
+	z[_subckt->size()] = -1;
 	return z;
 }
 
@@ -106,17 +132,19 @@ const SubCkt SubCkt::operator/(const SubCkt& b) const {
 	}
 	return sc;
 }
-
+int SubCkt::in(unsigned int tgt) {
+	std::vector<int>::iterator is = find(_subckt->begin(), _subckt->end(), tgt);
+	if (is == _subckt->end()) {
+		return -1;
+	} else {
+		return std::distance(_subckt->begin(), is);
+	}
+}
 SubCkt::SubCkt(const SubCkt& other) : _ckt(other._ckt){
 	_levels = new std::vector<int>();
 	_subckt = new std::vector<int>();
 	this->_ref_node = other._ref_node;
+	this->_gpu = NULL;
 	this->_subckt->assign(other._subckt->begin(), other._subckt->end());
 	this->_levels->assign(other._levels->begin(), other._levels->end());
-}
-SubCkt& SubCkt::operator=(const SubCkt& other) { 
-	this->_ref_node = other._ref_node;
-	this->_levels = other._levels;
-	this->_subckt = other._subckt;
-	return *this;
 }
--- subckt.h
+++ subckt.h
@@ -3,28 +3,46 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <iterator>
 #include "ckt.h"
 class SubCkt { 
 	private:
 		const Circuit& _ckt;
 		int _ref_node;
+		int *_flat;
+		int *_gpu;
+		int* flat(); // get the flat array representation, allocates memory.
 		std::vector<int>* _levels;
 		std::vector<int>* _subckt;
+		void levelize();
 
 		void grow_recurse_back(unsigned int node);
 		void grow_recurse_forward(unsigned int node);
 	public:	
 		~SubCkt();
-		std::string save();
+		std::string save() const;
 		void load(const std::string& memfile);
 		SubCkt(const Circuit& ckt);
 		SubCkt(const SubCkt&);
+		void clear();
 		SubCkt(const Circuit& ckt, unsigned int node);
 		void add(const int& n) { add(this->_ckt, n);}
 		void add(const Circuit&, const int&);
-		int* gpu(); // get the flat array representation, allocates memory.
+		void copy();
+		bool operator<(const SubCkt&) const;
+		bool operator<(const int) const;
+
+		int* gpu() { 
+			if (this->_gpu == NULL) {
+				this->copy();
+			}
+			return this->_gpu;
+		}
 		int at(unsigned int);
+		int in(unsigned int);
 		int levelsize(unsigned int);
+		int levels();
+		int size() const { return this->_subckt->size();}
 		std::vector<int>& subckt() const { return *_subckt; }
 		const SubCkt operator/(const SubCkt& b) const; // intersection
 		SubCkt& operator=(const SubCkt&);
