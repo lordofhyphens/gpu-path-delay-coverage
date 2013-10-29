c9713ed56e7600ab981e66ff3ea396f07ad4f12e
Joseph Lenox <lenox.joseph@gmail.com>
2013-10-29 17:04:13 -0500

    Revert "removed extraneous files."
    This reverts commit 7d9c53a90543edfd5f103e73237f979b2a8ce98b.

---
+++ simkernel.h
@@ -0,0 +1,18 @@
+#ifndef SIMKERNEL_H
+#define SIMKERNEL_H
+#include <cassert>
+#include "util/gpuckt.h"
+#include "util/gpudata.h"
+#include "util/defines.h"
+#include "util/utility.h"
+#include <iomanip>
+#include <ctime>
+#include <string>
+#include <algorithm>
+#include <stdint.h>
+float gpuRunSimulation(GPU_Data& results, GPU_Data& inputs, GPU_Circuit& ckt, size_t chunk, size_t startPattern = 0, bool last = false);
+
+void debugSimulationOutput(ARRAY2D<uint8_t> results, std::string outfile);
+void debugSimulationOutput(GPU_Data* results, const GPU_Circuit& ckt, const size_t chunk, const size_t startPattern,std::string outfile);
+
+#endif
